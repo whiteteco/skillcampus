13b40b8b4eb4d63cbc3e9d999b8aacfca9142365
whiteteco <teco@mokeruto.jp>
2024-09-04 09:24:01 +0900

    .envw設定

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # .envファイルの内容を読み込む
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def map():
-    return render_template("index.html")
+    google_maps_api_key = os.getenv("GOOGLE_MAPS_API_KEY")
+    return render_template("index.html", api_key=google_maps_api_key)
 
 
 if __name__ == "__main__":
